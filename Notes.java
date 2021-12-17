4cc21a698c123ad2b5ed8b8ce39f8a0a0c9165a5
Eganathan R <ammasamrith@gmail.com>
2021-12-18 01:27:00 +0530

    Update Notes.java

--- Notes.java
+++ Notes.java
@@ -3,8 +3,8 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Collection;
 import java.util.HashMap;
-
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,142 +13,162 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
-
 @SuppressWarnings("serial")
-public class Notes extends JFrame implements ActionListener, ListSelectionListener {
-	
+public class Notes extends JFrame implements ActionListener, ListSelectionListener, TextChangeTemp {
+
 	Color sColor = Color.getHSBColor(80, 40, 30); //
-	
+
 	private JButton nFrameBtn;
 	static String title = "Sticky Notes";
-	
-	public static DefaultListModel<String> leafsM;
-	static int counter;
-	
-	static HashMap<String,Leafs> Objmap =new HashMap<String,Leafs>();
-	static private JList<String> list;
-	
-	Notes() {
-		
-		JFrame x = new JFrame(title);
-		x.setSize(300,600);
-		x.setLocation(50,60);
-		x.setLayout(new BorderLayout());;
-		
-		nFrameBtn = new JButton("New Note"); // Main frame title 
-		nFrameBtn.setBackground(sColor); // sets the background
-		nFrameBtn.setFont( new Font("sans" ,Font.BOLD, 24)); // seting the fonts
-		nFrameBtn.addActionListener(this); // ading action listener to new note button
+	public static DefaultListModel<String> listModal;
+	static int counter = 1;
 
-		
-		//List Model 
-		 leafsM = new DefaultListModel<>();    
-         //l1.addElement("Item4");
-         
-		 //J List 
-         list = new JList<>(leafsM);
-         list.setBorder(new EmptyBorder(10,10, 10, 10));
-         list.addListSelectionListener(this);
-         list.setBounds(100,100, 75,75);  
-         
-         //Ading the list
-         x.add(list,BorderLayout.CENTER);  
-		
-		
-		x.add(nFrameBtn , BorderLayout.NORTH);
-		
+	private HashMap<Integer, String> textHMap = new HashMap<Integer, String>();
+	private HashMap<Integer, String> titleHMap = new HashMap<Integer, String>();
+	private HashMap<Integer, String> creationHMap = new HashMap<Integer, String>();
+
+	private JList<String> list;
+
+	Notes() {
+
+		JFrame x = new JFrame(title);
+		x.setSize(300, 600);
+		x.setLocation(50, 60);
+		x.setLayout(new BorderLayout());
+		;
+
+		nFrameBtn = new JButton("New Note"); // Main frame title
+		nFrameBtn.setBackground(sColor); // sets the background
+		nFrameBtn.setFont(new Font("sans", Font.BOLD, 24)); // setting the fonts
+		nFrameBtn.addActionListener(this); // adding action listener to new note button
+
+		// List Model
+		listModal = new DefaultListModel<>();
+		// listModal.addAll(XML.getTitleList());
+		// J List
+		list = new JList<>(listModal);
+		list.setBorder(new EmptyBorder(10, 10, 10, 10));
+		list.addListSelectionListener(this);
+		list.setBounds(100, 100, 75, 75);
+
+		// Adding the list
+		x.add(list, BorderLayout.CENTER);
+
+		x.add(nFrameBtn, BorderLayout.NORTH);
+
 		x.setVisible(true);
 		x.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		
 	}
-	
+
 	/*
-	 * Creates a new instance of sticky note,
-	 * adds them to the Hash map with the key as the file name and stores the instance there
-	 * adds to the List as well
+	 * Creates a new instance of sticky note, creates a unique ID, fileName ,
+	 * default text adds the ID to all the Hash maps adds the temporary title to the
+	 * hash map and List Modal
+	 * 
+	 * @param idName is a unique id with counter
+	 * 
+	 * @param newFileName is a temporary title
+	 * 
+	 * @param defaultText is the text for the Text area currently its empty
 	 */
-	static void newNote(){
-		//Setting a file value with counter
-		String lName = "My Note 0"+counter;
-		
-		//leafs Object 
-		Leafs Object = new Leafs(lName);
-		
-		//adding to leafsM
-		//leafsM.addElement(lName);
-		//adding to HMap with key and value 
-		Objmap.put(lName, Object);
-		leafsM.addElement(lName);
-		//list.add(lName, new JButton(lName));
-		
-		;
+	private void newNote() {
+
+		int idName = counter;
+		String newFileName = "";
+		String defaultText = "";
+
+		// Title Generation for new note
+		if (counter <= 9) // checking if the counter is <= 9 then the counter will be 09 , 08, 01 ..etc
+		{
+			newFileName = "My Note 0" + counter;
+		} else // if higher than 9 then the counter will be 10,11,12...etc
+		{
+			newFileName = "My Note " + counter;
+		}
+
+		// creating anonymous new note here with the parameters
+		Leafs newNote = new Leafs(idName, newFileName, defaultText, this);
+		newNote.leafFrame.setVisible(true);
+
+		titleHMap.put(idName, newFileName); // updating the titleHMap with ID and title
+		textHMap.put(idName, defaultText); // updating the textHMap with ID and default text
+		listModal.addElement(newFileName);
+		counter++; // incrementing after the object is created successfully
 	}
-	
-	
-	/*static void saveNote(){
-		 File notesFolder = new File(System.getProperty("user.home") + "/.sticky_notes/notes");
-	        notesFolder.mkdirs();
-	        File[] notesFiles = notesFolder.listFiles();
-	}*/
-	
-	
-	public void upDateList(){
-		
-		String tempKey = list.getSelectedValue();
-		
-		if(Objmap.containsKey(list.getSelectedValue())){
-			
-				Objmap.get(list.getSelectedValue()).leafFrame.setVisible(true);
-				Objmap.get(list.getSelectedValue()).upDateTitle();
-				
-				String newKey = Objmap.get(list.getSelectedValue()).upDateTitle();
-				Objmap.put(newKey, Objmap.get(list.getSelectedValue()));
-				
-				leafsM.remove(list.getSelectedIndex());
-				leafsM.addElement(newKey);
-				list.repaint();
-				
-				Objmap.remove(tempKey);				
-				System.out.println(Objmap + "inside upDateList()");}
-		
-		
+
+	private void openNote(int ID, String title, String text) {
+
+		// creating anonymous new note here with the parameters
+		Leafs openNote = new Leafs(ID, title, text, this);
+		openNote.leafFrame.setVisible(true);
 	}
-		
+
+	public void updateListModal() {
+		listModal.removeAllElements();
+		listModal.addAll(titleHMap.values());
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == nFrameBtn) {
-			
-			counter++;
-			newNote();		
-		}}
+		if (e.getSource() == nFrameBtn) //
+		{
+			// calling the method to create a new note with custom parameters
+			newNote();
+
+		}
+	}
 
 	@Override
 	/*
-	 * List selection event 
+	 * List selection event
 	 * 
 	 */
 	public void valueChanged(ListSelectionEvent e) {
-		
-		String tempKey = list.getSelectedValue();
-		
-		if(Objmap.containsKey(list.getSelectedValue())){
-			
-			Objmap.get(list.getSelectedValue()).upDateTitle(); // updates the Title if the sLen is greater than available 5  
-			Objmap.get(list.getSelectedValue()).leafFrame.setVisible(true); //Sets the objects visibility to true
-			
-			
-			String newKey = Objmap.get(list.getSelectedValue()).upDateTitle();
-			Objmap.put(newKey, Objmap.get(list.getSelectedValue()));
-			
-			leafsM.remove(list.getSelectedIndex());
-			leafsM.addElement(newKey);
-			list.repaint();
-			
-			Objmap.remove(tempKey);
+
+		String currKey = list.getSelectedValue();
+		if (list.getValueIsAdjusting() && titleHMap.containsValue(currKey)) {
+			for (int id : titleHMap.keySet()) {
+				if (currKey == titleHMap.get(id)) {
+					openNote(id, titleHMap.get(id), textHMap.get(id));
+				}
 			}
-		
+
+			// System.out.println(titleHMap. + "selected ID ");
+		} else {
+
+		}
 	}
-	
+
+	@Override
+	public void OnTextChanged(String text, int id) {
+		// TODO Auto-generated method stub
+		updateText(id, text);
+		// System.out.println(text + " | on the ID ->:" + id);
+	}
+
+	/*
+	 * gets Invoked every time a key is pressed updates Title to the title hash map
+	 */
+	private void updateTitle(int ID, String text) {
+		String title = text;
+
+		if (text.length() >= 12) // in case the length is larger that 12 [MAX TITLE]
+		{
+			text.substring(0, 12);
+		}
+		titleHMap.replace(ID, title);
+		// System.out.println(titleHMap.values() +"** Titles **");
+		updateListModal();
+	}
+
+	/*
+	 * gets Invoked every time a key is pressed updates the text from the frame
+	 */
+	private void updateText(int ID, String text) // updates the value to the hash map
+	{
+		textHMap.replace(ID, text);
+		updateTitle(ID, text);
+		// System.out.println(textHMap.values()+"** Text **");
+	}
 
 }
