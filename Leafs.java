fa3d393e1c0d2b42987852b825a6ff9725fe4005
Eganathan R <ammasamrith@gmail.com>
2022-01-04 10:13:42 +0530

    Update Leafs.java

--- Leafs.java
+++ Leafs.java
@@ -8,9 +8,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.font.TextAttribute;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import javax.imageio.ImageIO;
 import javax.swing.Icon;
@@ -19,6 +24,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
@@ -42,13 +48,16 @@ public class Leafs implements ActionListener, KeyListener {
 	JButton deleteBtn;
 	JButton saveBtn;
 	
+	private HashMap<Integer,Integer> boldHMap = new HashMap<>();
+	private HashMap<String,String> italicHmap = new HashMap<>();
+	
 	// Font Variables
 	int dFontDize = 12;
 	int dStyle = 0; // 0 -> plain , 1 -> BOLD , 3 -> italic
 	String fFamily = "Sans";
 	Font dFont = new Font(fFamily, dStyle, dFontDize);
 	Font btnFonts = new Font(fFamily, 0, dFontDize); // style of buttons
-
+	Font taFont = new Font(fFamily, 0, 16);
 	// Bold font for Button
 	Font bFont = new Font(fFamily, Font.BOLD, dFontDize);
 	Font iFont = new Font(fFamily, Font.ITALIC, dFontDize);
@@ -92,14 +101,18 @@ public class Leafs implements ActionListener, KeyListener {
 		leafFrame.setLocation(450, 50);
 
 		tArea = new JTextPane();
-		tArea.setFont(dFont);
+		tArea.setFont(taFont);
 		tArea.setText(textValue);
-		// tArea.setRequestFocusEnabled(rootPaneCheckingEnabled);
 		tArea.setBackground(dColor);
 		tArea.setBorder(new EmptyBorder(10, 10, 10, 10));
 		tArea.addKeyListener(this);
 		tArea.setSize(leafFrame.getWidth(), leafFrame.getHeight());
 
+		JScrollPane areaScrollPane = new JScrollPane(tArea);
+		areaScrollPane.setVerticalScrollBarPolicy(
+		                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		//areaScrollPane.setPreferredSize(new Dimension(250, 250));
+		
 		ePanel = new JPanel();
 		ePanel.setLayout(new BorderLayout());
 		
@@ -107,22 +120,41 @@ public class Leafs implements ActionListener, KeyListener {
 		eBtnPanel.setBackground(sColor);
 		eBtnPanel.setSize(leafFrame.getWidth(), 80);
 		eBtnPanel.setLayout(new GridLayout(1, 5));
+		
+		//Strike through FONT
+		Font font = new Font("helvetica", Font.PLAIN, 12);
+		Map  attributes = font.getAttributes();
+		attributes.put(TextAttribute.STRIKETHROUGH, TextAttribute.STRIKETHROUGH_ON);
+		Font newFont = new Font(attributes);
+		
+		//Underline FONT btn
+		Font fontU = new Font("helvetica", Font.PLAIN, 12);
+		Map  attributesU = fontU.getAttributes();
+		attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+		Font uFont = new Font(attributesU);
 
 		italicBtn = new JButton("i");
 		boldBtn = new JButton("B");
-		upperCaseBtn = new JButton("U");
-		underLineBtn = new JButton("V");
 		strikeThroughBtn = new JButton("S");
+		//upperCaseBtn = new JButton("U");
+		underLineBtn = new JButton("U");
+		
+		//strikeThroughBtn.setFont(newFont);
 
 		// sets Style and
 		// adds Action Listener to all the Button instances
 		defaultStyle(italicBtn);
 		italicBtn.setFont(iFont);
+		
 		defaultStyle(boldBtn);
-		defaultStyle(upperCaseBtn);
+		
 		defaultStyle(underLineBtn);
-		underLineBtn.setFont(hBFont);
+		underLineBtn.setFont(uFont);
+		
+		
 		defaultStyle(strikeThroughBtn);
+		strikeThroughBtn.setFont(newFont);
+		
 		//END of Edit Panel 
 		
 		//update panel
@@ -215,13 +247,9 @@ public class Leafs implements ActionListener, KeyListener {
 	void editText(char action, char bol) {
 
 		SimpleAttributeSet attr = new SimpleAttributeSet();
-
-		// Index of the edited text and its value
-		// int startTextRef = tArea.getSelectionStart(); //Start of the index
-		// int endTextRef = tArea.getSelectionEnd(); // end of the index
-		// String cText = tArea.getSelectedText(); // current text from the entry
-		// System.out.println(startTextRef +" "+endTextRef +" "+cText);
-
+		
+		int startAtt = 0;
+		int endAtt = 0;
 		// START OF BOLD
 		if (action == 'b') {
 			// BOLD ACTION
@@ -232,7 +260,14 @@ public class Leafs implements ActionListener, KeyListener {
 				StyleConstants.setBold(attr, true);
 				tArea.setCharacterAttributes(attr, true);
 				boldBtn.repaint();
-				System.out.println(styledDocument);
+				
+				//int caretOffset = tArea.getCaretPosition();
+				//int lineNumber = tArea.getLineOfOffset(caretOffset);
+				//int startOffset = tArea.getLineStartOffset(lineNumber);
+				//int endOffset = tArea.getLineEndOffset(lineNumber);
+				startAtt = tArea.getCaretPosition();
+
+				
 
 				if (italicBtnActive) {
 					StyleConstants.setItalic(attr, true);
@@ -241,6 +276,8 @@ public class Leafs implements ActionListener, KeyListener {
 				boldBtn.setBackground(sColor);
 				boldBtn.setFont(btnFonts);
 				StyleConstants.setBold(attr, false);
+				endAtt = tArea.getCaretPosition();
+				boolean x = startAtt >= 0 ? addBoldIndexToMap(startAtt,endAtt):false;
 			}
 			tArea.setCharacterAttributes(attr, false);
 			attr.removeAttributes(attr);
@@ -276,7 +313,12 @@ public class Leafs implements ActionListener, KeyListener {
 	public void actionPerformed(ActionEvent e) {
 
 		if (e.getSource() == boldBtn) {
-
+			
+			//System.out.println(tArea.getCaretPosition());
+			//tArea.setText("*");
+			
+			
+			
 			// BOLD BTN ACTION
 			if (boldBtnActive == false) {
 				editText('b', 't');
@@ -306,13 +348,21 @@ public class Leafs implements ActionListener, KeyListener {
 			Notes.dataBase.deleteRowWithID(currID);
 			closeOperation();
 		}else if (e.getSource() == saveBtn ) {
+			
 			//saveBtn
-			Notes.dataBase.updateTextAndTitle(currID, tArea.getText().toString().toString(), tArea.getText().toString());
+			Notes.dataBase.updateTextAndTitle(currID, tArea.getText().toString(), tArea.getText().toString());
+			new DB().loadDataFromDB();
+			Notes.updateListModal();
 		}
 
 		// END OF ACTION PERFORMED METH
 
 		// END OF LEAF CLASS
+	}
+	
+	String genTitle() {
+		
+		return tArea.getText().toString().substring(0,tArea.getText().length()-1 <= 22 && tArea.getText().length()-1 > 1 ? tArea.getText().length()-1:21);
 	}
 
 	@Override
@@ -325,7 +375,8 @@ public class Leafs implements ActionListener, KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+		//TESTING
+		//System.out.println(styledDocument.getProperty(e).toString());
 
 	}
 
@@ -343,19 +394,47 @@ public class Leafs implements ActionListener, KeyListener {
 			char currChar = tArea.getText().charAt(len-1) ;
 		if(currChar  == ' ' || currChar == '.')
 		{
-		
-		Notes.dataBase.updateTextAndTitle(currID, tArea.getText().toString(), tArea.getText().toString());
-		new DB().loadDataFromDB();
-		Notes.updateListModal();
+			
+		//Notes.dataBase.updateTextAndTitle(currID, genTitle(), tArea.getText().toString());
+		//leafFrame.setTitle(genTitle());
+		//new DB().loadDataFromDB();
+		//Notes.updateListModal();
 	}
 		if(len >= 5 && len <= 6) {
-			Notes.dataBase.updateTextAndTitle(currID, tArea.getText().toString(), tArea.getText().toString());
+			Notes.dataBase.updateTextAndTitle(currID, genTitle(), tArea.getText().toString());
 			new DB().loadDataFromDB();
 			Notes.updateListModal();
 		}
 		
 		}
 		
+		//System.out.println(tArea.);
 	}
+	
+	//adding the bold index
+	boolean addBoldIndexToMap(int start, int end) {
+		boolean isValid = start+end >= 1 ? true:false; 
+		boldHMap.put(start, end);
+		return isValid;
+	}
+	
+	//removing the bold index
+	boolean removeBoldIndexToMap(int start) {
+		boolean isValid = start >= 1 ? true:false; 
+		boldHMap.remove(start);
+		return isValid;
+	}
+	
+	//getting the start index
+	Set<Integer> getBoldStartIndexFromMap() {
+		boolean isValid = boldHMap.size() >= 1 ? true:false; 
+		return boldHMap.keySet();
+	}
+	
+	//Getting the ending
+		Collection<Integer> getBoldEndIndexFromMap() {
+			boolean isValid = boldHMap.size() >= 1 ? true:false; 
+			return boldHMap.values();
+		}
 
 }
